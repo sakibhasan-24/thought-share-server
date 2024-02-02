2fd8dd18652a10efe301cbc45e650ebfd16d0375
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-02-02 20:26:41 +0600

    create verify token and create post api

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -13,12 +18,67 @@ const client = new MongoClient(process.env.MONGO_URI, {
 });
 
 async function run() {
+  // collections
+  const usersCollection = client.db("blogs").collection("users");
+  const postsCollection = client.db("blogs").collection("posts");
+  //   end of collections
   try {
-    console.log("connected");
+    // token create and save
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN);
+      res.send({ token });
+    });
+    // end of token create and save
+    // verify token
+    const verifyToken = (req, res, next) => {
+      const headers = req.header.authorization;
+      console.log("verifyToken", headers);
+      if (!headers) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = headers.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(403).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+    // root route
+    app.get("/", async (req, res) => {
+      res.send("blogs server is running");
+    });
+
+    // user route
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      //   console.log(user);
+      const existingUser = await usersCollection.findOne({ email: user.email });
+      if (existingUser) {
+        return res.send({ success: false, error: "User already exists" });
+      }
+      try {
+        const result = await usersCollection.insertOne(user);
+        res.send({ success: true, data: result, user });
+      } catch (error) {
+        res.send({ success: false, error: error.message });
+      }
+    });
+    // end of user route
+    // create a post
+
+    app.post("/create-post", verifyToken, async (req, res) => {
+      const data = req.body;
+      const insertedData = { ...data, createdAt: new Date() };
+      const result = await postsCollection.insertOne(insertedData);
+      res.send(result);
+    });
   } finally {
   }
 }
 run().catch(console.dir);
-app.get("/", (req, res) => res.send("Hello Blogs"));
+// app.get("/", (req, res) => res.send("Hello Blogs"));
 app.listen(port, () => console.log(`port is running on ${port}!`));
 //
