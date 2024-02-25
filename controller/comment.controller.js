5a1756bf8de0409773a6d67bbbcd1558170f5e24
sakibhasan-24 <sakibhasan7724@gmail.com>
2024-02-26 02:27:32 +0600

    react function

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -41,3 +41,38 @@ export const getComments = async (req, res) => {
       .json({ message: "something went wrong", success: false });
   }
 };
+
+export const likeAction = async (req, res) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return res
+        .status(404)
+        .json({ message: "comment not found", success: false });
+    }
+    // comment exist
+    // if not liked then add like,if liked then remove
+    const isLiked = comment.likes.indexOf(req.user.id);
+    // console.log(req.user.id);
+    console.log(comment.likes);
+    if (isLiked === -1) {
+      //   console.log(comment.likes);
+      comment.likes.push(req.user.id);
+
+      comment.numberOfLikes += 1;
+    } else {
+      comment.likes.splice(isLiked, 1);
+      comment.numberOfLikes -= 1;
+    }
+    await comment.save();
+    return res.status(200).json({
+      success: true,
+      message: "like action done successfully",
+      comment,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "something went wrong", success: false });
+  }
+};
--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -3,11 +3,13 @@ import { verifyToken } from "../helper/verifyToken.js";
 import {
   createComment,
   getComments,
+  likeAction,
 } from "../controller/comment.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
 router.get("/getComments/:postId", getComments);
+router.put("/likeAction/:commentId", verifyToken, likeAction);
 
 export default router;
